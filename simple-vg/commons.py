ade217248e4c165af2d5999f6c90a5cf196348bc
coldyuja <trtr75131@gmail.com>
2024-12-04 03:47:09 +0900

    Added - Add method for processing input when elem is executed inside pipeline

--- simple-vg/commons.py
+++ simple-vg/commons.py
@@ -4,8 +4,15 @@ class AbstractPipelineElement:
     def __init__(self):
         return
 
+    def _process_input(self, input):
+        raise NotImplementedError(NOT_IMPL_ERR_MSG)
+
     def _execute(self):
         raise NotImplementedError(NOT_IMPL_ERR_MSG)
 
     def get_result(self):
         raise NotImplementedError(NOT_IMPL_ERR_MSG)
+    
+    def _process(self):
+        self._execute()
+        return self.get_result()
