3a3346eba176865e861818e1f8e4e9a395b5c1f1
coldyuja <trtr75131@gmail.com>
2024-12-13 03:08:09 +0900

    Added - Impl separation using speech_separation.sepreformer

--- simple_vg/process_overlap.py
+++ simple_vg/process_overlap.py
@@ -1,5 +1,8 @@
-from typing import TypedDict
+from enum import Enum
+from typing import Dict, TypedDict
 from commons import AbstractPipelineElement, ModelWrapper
+from speech_separation.sepreformer import SepReformerModels, SepReformerSetting, SepReformerWrapper
+from vg_types import AudioSetting
 
 # https://www.isca-archive.org/interspeech_2023/yu23c_interspeech.pdf
 # Detect Overlapped Speech
@@ -14,29 +17,58 @@ from commons import AbstractPipelineElement, ModelWrapper
 # SOTA of Speech Separation
 
 
+class ProcessOverlappedAudioTasks(Enum):
+    SEPARATE_AUDIO = 0
+    DETECT_OVERLAPPED = 1
+
+class SeparateOverlappedSettings(TypedDict):
+    model_settings: SepReformerSetting
+
+class DetectOverlappedSettings(TypedDict):
+    temp: str
+
+class ProcessOverlappedAudioSettings(TypedDict):
+    tasks: ProcessOverlappedAudioTasks
+    separation_settings: SeparateOverlappedSettings
+    detection_settings : DetectOverlappedSettings
+
+
 class ProcessOverlappedAudio(AbstractPipelineElement):
-    def __init__(self):
-        
+    def __init__(self, settings: AudioSetting):
+        self.settings: ProcessOverlappedAudioSettings = settings['opt_settings']
+        self.audio_settings = settings
         return
 
     def _detect_overlapped(self):
         return
     
     def _separate_overlapped_audio(self):
+        sep_name = self.settings['separation_settings']['model_settings'].__name__
+        match sep_name:
+            case 'SepReformerSettings':
+                self._use_sepreformer()
+            case _:
+                raise KeyError(f'Unknown Settings: {sep_name}')
         return
     
 
     def _process_input(self, input):
-        return super()._process_input(input)
+        self.input = input
     
     def _execute(self):
-        return super()._execute()
+        if self.settings['tasks'] == ProcessOverlappedAudioTasks.SEPARATE_AUDIO:
+            self._separate_overlapped_audio()    
+        
+        if self.settings['tasks'] == ProcessOverlappedAudioTasks.DETECT_OVERLAPPED:
+            self._detect_overlapped()
     
     def get_result(self):
-        return super().get_result()
+        return self.result
     
     def _use_sepreformer(self):
-        
-        return
+        self.model = SepReformerWrapper(self.input, self.settings['separation_settings']['model_settings'])
+        self.model.inference()
+        self.result = self.model.get_result()
+        return 
 
 
