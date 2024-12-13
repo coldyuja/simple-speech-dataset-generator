64d669c254886bed4fc7393d24066c8c1fb77edb
coldyuja <trtr75131@gmail.com>
2024-12-13 19:46:37 +0900

    Added & Removed - Added printing dbg, error info in _run_sequential() and removed some imports

--- simple_vg/pipeline.py
+++ simple_vg/pipeline.py
@@ -1,6 +1,8 @@
 from enum import Enum
 from typing import Any
-from commons import AbstractPipelineElement
+from .commons import AbstractPipelineElement
+from torch import Tensor
+import numpy as np
 
 class ExecuteType(Enum):
     NoInit = 0
@@ -16,25 +18,29 @@ class VGPipeline:
         self.run_type = ExecuteType.Sequential
         return
 
-    def run(self, input: Any):
+    def run(self, input: Any, dbg=False):
         match self.run_type:
             case ExecuteType.Sequential:
-                self._run_sequential(input)
+                self._run_sequential(input, dbg=dbg)
             case _:
                 raise KeyError('Not Supported ExecutionType or VGPipeline is not initialized properly!')
             
         return
     
     # I wanted to give type hints more detail, but [T] = Generic[T] does not supported python 3.10
-    # If i use Generic[T], code will be messy. so that's why i didnt this type hinting
-    def _run_sequential(self, input: Any) -> Any:
-        mid_ret = None
+    # If i use Generic[T], code will be messy. so that's why i didnt type hinting on this method
+    def _run_sequential(self, input: Any, dbg=False) -> Any:
+        mid_ret = input
         for i, p in enumerate(self.seq_pipes):
+            p_name = p.__class__.__qualname__
             try:
-                p._process_input(input)
+                p._process_input(mid_ret)
                 p._execute()
                 mid_ret = p.get_result()
+                if dbg:
+                    print(f'Index-{i} {p_name} Pipeline Output: {mid_ret.__class__.__qualname__}, shape: {mid_ret.shape if isinstance(mid_ret, (Tensor, np.ndarray)) else None}')
             except Exception as e:
-                print(f'Exception Occurred during processing pipeline. \nIndex={i} \nPipeline={p.__name__} \nError:\n{e}\n')
-
+                print(f'Exception Occurred during processing pipeline. \nIndex={i} \nPipeline={p_name}\n')
+                raise e
         return mid_ret
+    
