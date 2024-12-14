6878f29bffbcfc5ca113dd1017d5f05f037f4cfd
coldyuja <trtr75131@gmail.com>
2024-12-14 15:27:45 +0900

    Added - mark for fitting to project

--- simple_vg/stt/whisper.py
+++ simple_vg/stt/whisper.py
@@ -25,76 +25,7 @@ def transcribe(
     hallucination_silence_threshold: Optional[float] = None,
     **decode_options,
 ):
-    """
-    Transcribe an audio file using Whisper
-
-    Parameters
-    ----------
-    model: Whisper
-        The Whisper model instance
-
-    audio: Union[str, np.ndarray, torch.Tensor]
-        The path to the audio file to open, or the audio waveform
-
-    verbose: bool
-        Whether to display the text being decoded to the console. If True, displays all the details,
-        If False, displays minimal details. If None, does not display anything
-
-    temperature: Union[float, Tuple[float, ...]]
-        Temperature for sampling. It can be a tuple of temperatures, which will be successively used
-        upon failures according to either `compression_ratio_threshold` or `logprob_threshold`.
-
-    compression_ratio_threshold: float
-        If the gzip compression ratio is above this value, treat as failed
-
-    logprob_threshold: float
-        If the average log probability over sampled tokens is below this value, treat as failed
-
-    no_speech_threshold: float
-        If the no_speech probability is higher than this value AND the average log probability
-        over sampled tokens is below `logprob_threshold`, consider the segment as silent
-
-    condition_on_previous_text: bool
-        if True, the previous output of the model is provided as a prompt for the next window;
-        disabling may make the text inconsistent across windows, but the model becomes less prone to
-        getting stuck in a failure loop, such as repetition looping or timestamps going out of sync.
-
-    word_timestamps: bool
-        Extract word-level timestamps using the cross-attention pattern and dynamic time warping,
-        and include the timestamps for each word in each segment.
-
-    prepend_punctuations: str
-        If word_timestamps is True, merge these punctuation symbols with the next word
-
-    append_punctuations: str
-        If word_timestamps is True, merge these punctuation symbols with the previous word
-
-    initial_prompt: Optional[str]
-        Optional text to provide as a prompt for the first window. This can be used to provide, or
-        "prompt-engineer" a context for transcription, e.g. custom vocabularies or proper nouns
-        to make it more likely to predict those word correctly.
-
-    carry_initial_prompt: bool
-        If carry_initial_prompt is True, `initial_prompt` is prepended to the prompt of each internal
-        `decode()` call. If there is not enough context space at the start of the prompt, it is
-        left-sliced to make space.
-
-    decode_options: dict
-        Keyword arguments to construct `DecodingOptions` instances
-
-    clip_timestamps: Union[str, List[float]]
-        Comma-separated list start,end,start,end,... timestamps (in seconds) of clips to process.
-        The last end timestamp defaults to the end of the file.
-
-    hallucination_silence_threshold: Optional[float]
-        When word_timestamps is True, skip silent periods longer than this threshold (in seconds)
-        when a possible hallucination is detected
-
-    Returns
-    -------
-    A dictionary containing the resulting text ("text") and segment-level details ("segments"), and
-    the spoken language ("language"), which is detected when `decode_options["language"]` is None.
-    """
+    # Setting precision fp16/32 -> ign
     dtype = torch.float16 if decode_options.get("fp16", True) else torch.float32
     if model.device == torch.device("cpu"):
         if torch.cuda.is_available():
@@ -104,13 +35,14 @@ def transcribe(
             dtype = torch.float32
 
     if dtype == torch.float32:
-        decode_options["fp16"] = False
+        decode_options["fp16"] = False 
 
-    # Pad 30-seconds of silence to the input audio, for slicing
+    # Pad 30-seconds of silence to the input audio, for slicing -> mov DatasetCode; alt mel, sample
     mel = log_mel_spectrogram(audio, model.dims.n_mels, padding=N_SAMPLES)
     content_frames = mel.shape[-1] - N_FRAMES
     content_duration = float(content_frames * HOP_LENGTH / SAMPLE_RATE)
 
+    # decoder options about laguages -> mov ExtractText_elem
     if decode_options.get("language", None) is None:
         if not model.is_multilingual:
             decode_options["language"] = "en"
@@ -119,6 +51,7 @@ def transcribe(
                 print(
                     "Detecting language using up to the first 30 seconds. Use `--language` to specify the language"
                 )
+            # ign pad_or_trim
             mel_segment = pad_or_trim(mel, N_FRAMES).to(model.device).to(dtype)
             _, probs = model.detect_language(mel_segment)
             decode_options["language"] = max(probs, key=probs.get)
@@ -126,16 +59,18 @@ def transcribe(
                 print(
                     f"Detected language: {LANGUAGES[decode_options['language']].title()}"
                 )
-
+    # mov fn_arg 
     language: str = decode_options["language"]
     task: str = decode_options.get("task", "transcribe")
+
+    # impl this
     tokenizer = get_tokenizer(
         model.is_multilingual,
         num_languages=model.num_languages,
         language=language,
         task=task,
     )
-
+    # External timestamps -> ign
     if isinstance(clip_timestamps, str):
         clip_timestamps = [
             float(ts) for ts in (clip_timestamps.split(",") if clip_timestamps else [])
@@ -145,20 +80,27 @@ def transcribe(
         seek_points.append(0)
     if len(seek_points) % 2 == 1:
         seek_points.append(content_frames)
+    # same as dataset impl -> ign
     seek_clips: List[Tuple[int, int]] = list(zip(seek_points[::2], seek_points[1::2]))
 
+    # ign
     punctuation = "\"'“¿([{-\"'.。,，!！?？:：”)]}、"
 
+    # ign
     if word_timestamps and task == "translate":
         warnings.warn("Word-level timestamps on translations may not be reliable.")
 
+    # may pull it to root -> lazy
     def decode_with_fallback(segment: torch.Tensor) -> DecodingResult:
+        # for single value -> ign
+        # default temp: (0.0, 0.2, 0.4, 0.6, 0.8, 1.0)
         temperatures = (
             [temperature] if isinstance(temperature, (int, float)) else temperature
         )
         decode_result = None
 
         for t in temperatures:
+            # Optional decoder options -> ign
             kwargs = {**decode_options}
             if t > 0:
                 # disable beam_size and patience when t > 0
@@ -169,8 +111,11 @@ def transcribe(
                 kwargs.pop("best_of", None)
 
             options = DecodingOptions(**kwargs, temperature=t)
+
+            # code flow may need to alter -> lazy
             decode_result = model.decode(segment, options)
 
+            # ign
             needs_fallback = False
             if (
                 compression_ratio_threshold is not None
@@ -196,16 +141,20 @@ def transcribe(
 
     clip_idx = 0
     seek = seek_clips[clip_idx][0]
+    # need to analyze log_mel_spec..() -> lazy
     input_stride = exact_div(
         N_FRAMES, model.dims.n_audio_ctx
     )  # mel frames per output token: 2
     time_precision = (
+        # need import HOP_LENGTH -> mov this_mod ExtractTextSettings
         input_stride * HOP_LENGTH / SAMPLE_RATE
     )  # time per output token: 0.02 (seconds)
+
     all_tokens = []
     all_segments = []
     prompt_reset_since = 0
 
+    # .. -> lazy
     remaining_prompt_length = model.dims.n_text_ctx // 2 - 1
     if initial_prompt is not None:
         initial_prompt_tokens = tokenizer.encode(" " + initial_prompt.strip())
@@ -214,6 +163,7 @@ def transcribe(
     else:
         initial_prompt_tokens = []
 
+    # may be needed? -> ign
     def new_segment(
         *, start: float, end: float, tokens: torch.Tensor, result: DecodingResult
     ):
@@ -231,12 +181,13 @@ def transcribe(
             "no_speech_prob": result.no_speech_prob,
         }
 
-    # show the progress bar when verbose is False (if True, transcribed text will be printed)
+    # show the progress bar when verbose is False (if True, transcribed text will be printed) -> and 0
     with tqdm.tqdm(
         total=content_frames, unit="frames", disable=verbose is not False
     ) as pbar:
+        # ign
         last_speech_timestamp = 0.0
-        # NOTE: This loop is obscurely flattened to make the diff readable.
+        # NOTE: This loop is obscurely flattened to make the diff readable. -> what?
         # A later commit should turn this into a simpler nested loop.
         # for seek_clip_start, seek_clip_end in seek_clips:
         #     while seek < seek_clip_end
@@ -244,11 +195,17 @@ def transcribe(
             seek_clip_start, seek_clip_end = seek_clips[clip_idx]
             if seek < seek_clip_start:
                 seek = seek_clip_start
+
+            # out of idx -> lazy
             if seek >= seek_clip_end:
                 clip_idx += 1
                 if clip_idx < len(seek_clips):
+                    
                     seek = seek_clips[clip_idx][0]
                 continue
+
+            # some process being done by hop length(overlap ffts) -> ign
+            # goto ExtractTextDataset ; alt apply_hop_length
             time_offset = float(seek * HOP_LENGTH / SAMPLE_RATE)
             window_end_time = float((seek + N_FRAMES) * HOP_LENGTH / SAMPLE_RATE)
             segment_size = min(N_FRAMES, content_frames - seek, seek_clip_end - seek)
@@ -256,6 +213,7 @@ def transcribe(
             segment_duration = segment_size * HOP_LENGTH / SAMPLE_RATE
             mel_segment = pad_or_trim(mel_segment, N_FRAMES).to(model.device).to(dtype)
 
+            # ign
             if carry_initial_prompt:
                 nignored = max(len(initial_prompt_tokens), prompt_reset_since)
                 remaining_prompt = all_tokens[nignored:][-remaining_prompt_length:]
@@ -266,6 +224,7 @@ def transcribe(
             result: DecodingResult = decode_with_fallback(mel_segment)
             tokens = torch.tensor(result.tokens)
 
+            # ign
             if no_speech_threshold is not None:
                 # no voice activity check
                 should_skip = result.no_speech_prob > no_speech_threshold
@@ -283,7 +242,7 @@ def transcribe(
             previous_seek = seek
             current_segments = []
 
-            # anomalous words are very long/short/improbable
+            # anomalous words are very long/short/improbable -> ign
             def word_anomaly_score(word: dict) -> float:
                 probability = word.get("probability", 0.0)
                 duration = word["end"] - word["start"]
@@ -296,6 +255,7 @@ def transcribe(
                     score += duration - 2.0
                 return score
 
+            # ign
             def is_segment_anomaly(segment: Optional[dict]) -> bool:
                 if segment is None or not segment["words"]:
                     return False
@@ -304,6 +264,7 @@ def transcribe(
                 score = sum(word_anomaly_score(w) for w in words)
                 return score >= 3 or score + 0.01 >= len(words)
 
+            # ign
             def next_words_segment(segments: List[dict]) -> Optional[dict]:
                 return next((s for s in segments if s["words"]), None)
 
@@ -368,7 +329,8 @@ def transcribe(
                     )
                 )
                 seek += segment_size
-
+            
+            # ign block
             if word_timestamps:
                 add_word_timestamps(
                     segments=current_segments,
@@ -450,7 +412,7 @@ def transcribe(
                 for segment in current_segments:
                     start, end, text = segment["start"], segment["end"], segment["text"]
                     line = f"[{format_timestamp(start)} --> {format_timestamp(end)}] {text}"
-                    print(make_safe(line))
+                    print(make_safe(line)) # make_safe? -> lazy
 
             # if a segment is instantaneous or does not contain text, clear it
             for i, segment in enumerate(current_segments):
