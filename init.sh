a6001c8feed2c540d21120c686676886e3f08c42
coldyuja <trtr75131@gmail.com>
2024-12-15 03:54:17 +0900

    Deleted - not use

--- init.sh
+++
@@ -1,4 +0,0 @@
-sudo apt update && sudo apt upgrade
-sudo apt install ffmpeg
-pipx install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
-pipx install -r requirements.txt
